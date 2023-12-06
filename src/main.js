dbe5973affe20e1cb1a33b2e2854d2b56ee0a14e
kdingn <koukou3335@gmail.com>
2023-12-06 19:04:44 +0900

    add images

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import "vuetify/styles";
 
-const app = createApp(App)
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
-app.use(router)
-
-app.mount('#app')
+app = createApp(App).use(vuetify).mount("#app");
